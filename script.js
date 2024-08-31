9ec94c83909b3e1017e63fa692e3974f0d690e44
Gustavo Teixeira <teixeiragoesgustavo@gmail.com>
2024-08-31 11:34:47 -0300

    script.js

--- script.js
+++ script.js
@@ -1,11 +1,11 @@
-let veiculo1 = 'Captuh'
-let veiculo2 = 'Oroch'
-let veiculo1VL = 10
-let veiculo2VL = 10
-if (veiculo1VL > veiculo2VL) {
-    console.log(`O ${veiculo1} está a ${veiculo1VL}km , e está mais rapido que o ${veiculo2} \n, que está a ${veiculo2VL}km`)
-} else if (veiculo2VL > veiculo1VL) {
-    console.log(`o ${veiculo2} está a ${veiculo2VL}km , e está mais rapido que o ${veiculo1} \n , que está a ${veiculo1VL}`)
+let vehicle1 = 'Captuh'
+let vehicle2 = 'Oroch'
+let vehicle1VL = 10
+let vehicle2VL = 10
+if (vehicle1VL > vehicle2VL) {
+    console.log(`O ${vehicle1} está a ${vehicle1VL}km , e está mais rapido que o ${vehicle2} \n, que está a ${vehicle2VL}km`)
+} else if (vehicle2VL > vehicle1VL) {
+    console.log(`o ${vehicle2} está a ${vehicle2VL}km , e está mais rapido que o ${vehicle1} \n , que está a ${vehicle1VL}`)
 } else {
-    console.log(`tanto ${veiculo1} quanto ${veiculo2} estão na mesma velocidade de ${veiculo1VL}`)
+    console.log(`tanto ${vehicle1} quanto ${vehicle2} estão na mesma velocidade de ${vehicle1VL}`)
 }
